10266f8743e87441dcd761e45db9da971cf08e51
Jim <merajuljim@gmai.com>
2024-09-25 04:44:59 +0600

    all done! Alhamdulillah

--- js/events.js
+++ js/events.js
@@ -17,24 +17,26 @@ document.getElementById('btn-noakhali').addEventListener('click', function (){
         const availableBDT = document.getElementById('availableBDT').innerText;
         const collected = getBalanceText('collected-noakhali');
         const donatedBDT = getBalanceInput('input-noakhali');
-        const text = Number(collected) + Number(donatedBDT);  
-        setTheText(text, 'collected-noakhali');
-        const available = Number(availableBDT)-Number(donatedBDT);
-        setTheText(available, 'availableBDT');
+        const text = Number(collected) + Number(donatedBDT);
+        const available = Number(availableBDT)-Number(donatedBDT); 
+        if(donatedBDT == parseFloat(donatedBDT) && available >= 0 &&donatedBDT > 0){
+            setTheText(text, 'collected-noakhali');
+            setTheText(available, 'availableBDT');
         // add to history
         const newHistory = document.createElement('div');
-        newHistory.innerHTML = `<div class = "mx-2 my-3 border-inherit border-2">
-        <p>${donatedBDT} is donated for donate for flood at Noakhali, Bangladesh</p>
-         <div>
-             <p class='date'></p>
-         </div>
-         <script>
-            const time = new Date();
-            document.querySelector(".date").innerText = time;
-        </script>
+        newHistory.classList.add('mx-2', 'my-3', 'border-inherit', 'border-2');
+        const time = new Date();
+        const p = document.createElement('p');
+        p.innerHTML = `<p>${donatedBDT} is donated for donate for flood at Noakhali, Bangladesh</p>`
+        const textHistory = p.innerText;
+        const timeHistory = time.toString();
+        newHistory.innerText = textHistory+"\n"+timeHistory
+        document.getElementById('section-history').appendChild(newHistory);
+        my_modal_5.showModal();
+        }else{
+            alert('Invalid Donation amount');
+        }
         
-     </div>`
-     document.getElementById('section-history').appendChild(newHistory);
 })
 // -------------------------------------------------------------------------------------------------------------------
 // btn to donate feni
@@ -42,24 +44,25 @@ document.getElementById('btn-feni').addEventListener('click', function (){
     const availableBDT = document.getElementById('availableBDT').innerText;
     const collected = getBalanceText('collected-feni');
     const donatedBDT = getBalanceInput('input-feni');
-    const text = Number(collected) + Number(donatedBDT); 
-    setTheText(text, 'collected-feni');
+    const text = Number(collected) + Number(donatedBDT);
     const available = Number(availableBDT)-Number(donatedBDT);
-    setTheText(available, 'availableBDT');
+    if(donatedBDT == parseFloat(donatedBDT) && available >= 0 &&donatedBDT > 0){
+        setTheText(text, 'collected-feni');
+        setTheText(available, 'availableBDT');
     // add to history
     const newHistory = document.createElement('div');
-    newHistory.innerHTML = `<div class = "mx-2 my-3 border-inherit border-2">
-    <p>${donatedBDT} is donated for donate for flood at Feni, Bangladesh</p>
-     <div>
-         <p class='date'></p>
-     </div>
-     <script>
+        newHistory.classList.add('mx-2', 'my-3', 'border-inherit', 'border-2');
         const time = new Date();
-        document.querySelector(".date").innerText = time;
-    </script>
-    
- </div>`
- document.getElementById('section-history').appendChild(newHistory);  
+        const p = document.createElement('p');
+        p.innerHTML = `<p>${donatedBDT} is donated for donate for flood at Feni, Bangladesh</p>`
+        const textHistory = p.innerText;
+        const timeHistory = time.toString();
+        newHistory.innerText = textHistory+"\n"+timeHistory
+        document.getElementById('section-history').appendChild(newHistory);
+        my_modal_5.showModal();
+    }else{
+        alert('Invalid Donation amount');
+    }
 })
 // -------------------------------------------------------------------------------------------------------------------
 // btn to donate quota
@@ -67,24 +70,26 @@ document.getElementById('btn-quota').addEventListener('click', function (){
     const availableBDT = document.getElementById('availableBDT').innerText;
     const collected = getBalanceText('collected-quota');
     const donatedBDT = getBalanceInput('input-quota');
-    const text = Number(collected) + Number(donatedBDT); 
-    setTheText(text, 'collected-quota');
+    const text = Number(collected) + Number(donatedBDT);
     const available = Number(availableBDT)-Number(donatedBDT);
+    if(donatedBDT == parseFloat(donatedBDT) && available >= 0 &&donatedBDT > 0){
+        setTheText(text, 'collected-quota');
     setTheText(available, 'availableBDT');
     // add to history
     const newHistory = document.createElement('div');
-    newHistory.innerHTML = `<div class = "mx-2 my-3 border-inherit border-2">
-    <p>${donatedBDT} is donated for donate for injured in Quota Movement, Bangladesh</p>
-     <div>
-         <p class='date'></p>
-     </div>
-     <script>
+        newHistory.classList.add('mx-2', 'my-3', 'border-inherit', 'border-2');
         const time = new Date();
-        document.querySelector(".date").innerText = time;
-    </script>
-    
- </div>`
- document.getElementById('section-history').appendChild(newHistory);  
+        const p = document.createElement('p');
+        p.innerHTML = `<p>${donatedBDT} is donated for donate for injured students in Quota Movement, Bangladesh</p>`
+        const textHistory = p.innerText;
+        const timeHistory = time.toString();
+        newHistory.innerText = textHistory+"\n"+timeHistory
+        document.getElementById('section-history').appendChild(newHistory);
+        my_modal_5.showModal();
+    }else{
+        alert('Invalid Donation amount');
+    }
+      
 })
 // -------------------------------------------------------------------------------------------------------------------
 // btn donate 
@@ -100,4 +105,8 @@ document.getElementById('btn-history').addEventListener('click', function (){
     document.getElementById('btn-donate').classList.remove('bg-[#B4F461]');
     document.getElementById('section-history').classList.remove('hidden');
     document.getElementById('btn-history').classList.add('bg-[#B4F461]');
+})
+// btn blog
+document.getElementById('btn-to-blog').addEventListener('click', function (){
+    window.location.href = 'blog.html';
 })
---
+++ js/toHome.js
@@ -0,0 +1,4 @@
+// btn home 
+document.getElementById('btn-to-home').addEventListener('click', function (){
+    window.location.href = './index.html';
+})
